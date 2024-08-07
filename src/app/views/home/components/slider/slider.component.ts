6c5c19149b654909c95d4beaf78b2b2b7ce0bc14
Moustafa Amin <dev.moustafa.amin@gmail.com>
2024-08-07 17:38:20 +0300

    edit service language

--- src/app/views/home/components/slider/slider.component.ts
+++ src/app/views/home/components/slider/slider.component.ts
@@ -1,7 +1,5 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
-import { HttpClient } from '@angular/common/http';
 import { LanguageService } from 'src/app/Shared/services/language/language.service';
 
 @Component({
@@ -12,25 +10,38 @@ import { LanguageService } from 'src/app/Shared/services/language/language.servi
 export class SliderComponent implements OnInit {
   @Input() public homePage: any;
   currentLang: string;
+
+  customOptions!: OwlOptions;
+
   constructor(private languageService: LanguageService) {
     this.currentLang = this.languageService.getCurrentLang();
   }
 
-  customOptions: OwlOptions = {
-    loop: true,
-    margin: 0,
-    dots: true,
-    animateOut: 'fadeOut',
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-    },
-    nav: false,
-  };
+  ngOnInit(): void {
+    this.setOwlOptions(this.currentLang);
 
-  ngOnInit(): void {}
+    // Subscribe to language changes
+    this.languageService.languageChanged.subscribe((language: string) => {
+      this.setOwlOptions(language);
+    });
+  }
+
+  setOwlOptions(language: string): void {
+    this.customOptions = {
+      loop: true,
+      margin: 0,
+      dots: true,
+      animateOut: 'fadeOut',
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      rtl: language === 'ar',
+      responsive: {
+        0: {
+          items: 1,
+        },
+      },
+      nav: false,
+    };
+  }
 }
