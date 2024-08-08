d6fe068c6b283e2428c75a8381a1d172d7430fa0
Israa-Burhan <israaburhan97@gmail.com>
2024-08-08 15:12:22 +0300

    add dir to owl-carousel

--- src/app/views/home/components/discount-item/discount-item.component.ts
+++ src/app/views/home/components/discount-item/discount-item.component.ts
@@ -1,28 +1,45 @@
 import { OwlOptions } from 'ngx-owl-carousel-o';
-import { Component, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { LanguageService } from 'src/app/Shared/services/language/language.service';
 
 @Component({
   selector: 'app-discount-item',
   templateUrl: './discount-item.component.html',
   styleUrls: ['./discount-item.component.scss'],
 })
-export class DiscountItemComponent {
-  customOptions: OwlOptions = {
-    loop: true,
-    margin: 0,
-    dots: true,
-    animateOut: 'fadeOut',
-    autoplay: true,
-    autoplayTimeout: 10000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-    },
-    nav: false,
-  };
-
+export class DiscountItemComponent implements OnInit {
   @Input() public homePage: any;
+  currentLang: string;
+
+  customOptions!: OwlOptions;
+  constructor(private languageService: LanguageService) {
+    this.currentLang = this.languageService.getCurrentLang();
+  }
+
+  ngOnInit(): void {
+    this.setOwlOptions(this.currentLang);
+
+    // Subscribe to language changes
+    this.languageService.languageChanged.subscribe((language: string) => {
+      this.setOwlOptions(language);
+    });
+  }
+  setOwlOptions(language: string): void {
+    this.customOptions = {
+      loop: true,
+      margin: 0,
+      dots: true,
+      animateOut: 'fadeOut',
+      autoplay: true,
+      autoplayTimeout: 10000,
+      autoplayHoverPause: true,
+      rtl: language === 'ar',
+      responsive: {
+        0: {
+          items: 1,
+        },
+      },
+      nav: false,
+    };
+  }
 }
